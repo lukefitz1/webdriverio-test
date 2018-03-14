f0d1944b2ade82071ed3a948e440fcfae87e39d2
lukefitz1 <lukefitz1@gmail.com>
2018-03-13 22:25:56 -0400

    testing basic auth with webdriverio

---
+++ test_file.js
@@ -0,0 +1,21 @@
+var webdriverio = require('webdriverio');
+var options = {
+    headers: {
+      Authorization: 'Basic c3RvcmVmcm9udDpwYXNzNGJsdWVhY29ybg=='
+    },
+    desiredCapabilities: {
+        browserName: 'chrome'
+    }
+};
+
+webdriverio
+    .remote(options)
+    .init()
+    .url('https://storefront:pass4blueacorn@development.oreck.com')
+    .getTitle().then(function(title) {
+        console.log('Title was: ' + title);
+    })
+    .end()
+    .catch(function(err) {
+        console.log(err);
+    });
